034e2e19d0b5d51f916eed8588a2ab94551089fc
unknown <bb@bb-PC.(none)>
2011-11-15 23:05:08 +0200

    Initial commit.

---
+++ HRManagement.Test/HomeControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace HRManagement.Test
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void testingStuff()
+        {
+            Assert.True(true);
+        }
+    }
+}
